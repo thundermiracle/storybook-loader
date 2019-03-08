58a4a3affe7b23e5df600074d639c8dfc227bbfc
daniel <daniel@thundermiracle.com>
2019-03-08 21:33:13 +0900

    add google analytics

--- docs/.storybook/addons.js
+++ docs/.storybook/addons.js
@@ -1,2 +1,5 @@
 import '@storybook/addon-options/register';
 import '@storybook/addon-notes/register';
+import '@storybook/addon-google-analytics/register';
+
+window.STORYBOOK_GA_ID = 'UA-119418003-1';
