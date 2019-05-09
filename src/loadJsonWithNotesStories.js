87cbfcb96b045464ac48984da261c51e7d35461c
daniel <daniel@thundermiracle.com>
2019-05-09 22:44:58 +0900

    add typescript definitions to enhance  hints display  in VSCode

--- src/loadJsonWithNotesStories.js
+++ src/loadJsonWithNotesStories.js
@@ -2,13 +2,31 @@ import loadStories from './core/loadStories';
 import jsonLoader from './plugin/jsonLoader';
 import createMDThirdParamMaker from './plugin/createMDThirdParamMaker';
 
+/**
+ * @default
+ * loadStoriesOptions: {
+ * sort: true,
+ * sortFunc: (a, b) => a.localeCompare(b),
+ * noExt: true,
+ * groupByFolder: true,
+ * ignoreDotFolder: true,
+ * hierarchyRoot: '',
+ * folderNameWhenEmpty: 'ALL',
+ * dotFolderName: '.',
+ *}
+ */
 function loadJsonWithNotesStories(
   requireContext,
-  userOptions = {},
+  loadStoriesOptions = {},
   thirdParamMakerOptions = {},
 ) {
   const thirdParamMaker = createMDThirdParamMaker(thirdParamMakerOptions);
-  loadStories(jsonLoader, requireContext, { ...userOptions, thirdParamMaker }, true);
+  loadStories(
+    jsonLoader,
+    requireContext,
+    { ...loadStoriesOptions, thirdParamMaker },
+    true,
+  );
 }
 
 export default loadJsonWithNotesStories;
